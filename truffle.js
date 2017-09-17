0ee2efc14d907132fbdcda7ab57c5747b6e6b323
William Wnekowicz <william.wnekowicz@gmail.com>
2017-09-17 12:54:51 -0700

    Update the other truffle config. Doh.

--- truffle.js
+++ truffle.js
@@ -5,6 +5,12 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      host: "localhost",
+      port: 8545,
+      network_id: "3",
+      from: "0x3e26c527Ad4EeDacF08cD666F38E53267288A95d",
     }
   }
 };
